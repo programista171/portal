b252b59ddb074bd8f1a8e62a3ba2be464258ae80
kowalskidawid <kowalski69dawid@gmail.com>
2020-05-14 22:34:17 +0200

    database structure for chat, main view with messages

--- public/js/app.js
+++ public/js/app.js
@@ -37393,8 +37393,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Kamil\Desktop\programowanie\portal\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Kamil\Desktop\programowanie\portal\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/dawidkowalski/Desktop/Projects/portal/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/dawidkowalski/Desktop/Projects/portal/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
