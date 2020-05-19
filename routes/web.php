4f27dd9cca60585df14eaa52635f71b5ae0a3850
unknown <programista171@gmail.com>
2020-05-19 19:22:35 +0200

    Test

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return redirect("/login");
 });
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
 Route::post('/login/authenticate', 'Auth\LoginController@authenticate');
+
 
 Route::resource('/posts', 'PostsController');
 Route::post('/posts/createpost', 'PostsController@createPost');
@@ -31,3 +20,9 @@ Route::post('/friends/invite', 'FriendsController@invite');
 Route::get('/friends/{id}', 'FriendsController@listFriends');
 Route::get('/requests', 'FriendsController@listRequests');
 Route::post('/requests_decision', 'FriendsController@decide');
+Route::get("/messages", "Messages@index")->name("messages");
+
+Route::get('messagePusher', function () {
+    event(new \App\Events\MessagePushed());
+    return "event fired";
+});
