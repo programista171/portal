1aaf8d37b7834792114ae1e8a57cb1603677e0d2
unknown <programista171@gmail.com>
2020-05-19 16:24:03 +0200

    Routing improved

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,24 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/', function () {
-    return redirect("/login");
+    return view('welcome');
 });
 
 Auth::routes();
+
 Route::get('/home', 'HomeController@index')->name('home');
 Route::post('/login/authenticate', 'Auth\LoginController@authenticate');
 
@@ -20,9 +31,3 @@ Route::post('/friends/invite', 'FriendsController@invite');
 Route::get('/friends/{id}', 'FriendsController@listFriends');
 Route::get('/requests', 'FriendsController@listRequests');
 Route::post('/requests_decision', 'FriendsController@decide');
-Route::get("/messages", "Messages@index");
-
-Route::get('messagePusher', function () {
-    event(new \App\Events\MessagePushed());
-    return "event fired";
-});
