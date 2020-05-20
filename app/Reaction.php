ea8ebe392b44d4c334b491e0d82340acc78bcad1
Dawid Kowalski <kowalski69dawid@gmail.com>
2020-05-20 23:32:17 +0200

    add fillable properties to model
    Co-Authored-By: Dawid Kowalski <kontakt@kowalskidawid.pl>

--- app/Reaction.php
+++ app/Reaction.php
@@ -4,13 +4,20 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Reaction extends Model{
-	public function post(){
-		return $this->belongsTo(Post::class, 'postid');
-	}//endFunction
+class Reaction extends Model
+{
+    protected $fillable = [
+        "userId", "postId", "ispossitive"
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class, 'postid');
+    }//endFunction
 
 
-	public function user(){
-		return $this->belongsTo(User::class, 'userid');
-	}//endFunction
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userid');
+    }//endFunction
 }//endClass
