3ffa15bdf7e5976fea049af46ac5d1f1d1c87f7b
Dawid Kowalski <kowalski69dawid@gmail.com>
2020-05-24 21:55:51 +0200

    improve layout

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,29 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-<img alt="Zdjęcie profilowe" src="{{url('')}}/public/{{$user->profile->image}}">
-		<h2>Profil użytkownika {{$user->firstname}} {{$user->lastname}}</h2>
-		@if(Auth::user()->id != $user->id)
-@include('users.requests_decision')
-@else
-<a href="{{route("settings.edit")}}">Spersonalizuj swój profil</a>
-		@endif
-		<a href="{{url('/friends')}}/{{$user->id}}">Znajomi</a>
-<p>
-Imię: {{$user->firstname}}
-</p>
-<p>
-Nazwisko: {{$user->lastname}}
-</p>
-<p>
-Login: {{$user->login}}
-</p>
-<p>
-Email: {{$user->email}}
-</p>
-<p>
-Płeć: {{$user->profile->gender}}
-</p>
-<p>
-Opis: {{$user->profile->description}}</p>
+    <div class="card">
+        <div class="card-header row">
+            <div class="col-1">
+                <img alt="Zdjęcie profilowe" src="{{asset($user->profile->image)}}" class="img-thumbnail">
+            </div>
+            <h2 class="my-auto"> {{$user->firstname}} {{$user->lastname}}</h2>
+            <div class="ml-auto my-auto">
+                @if(Auth::user()->id != $user->id)
+                    @include('users.requests_decision')
+                @else
+                    <a href="{{route("settings.edit")}}">Spersonalizuj swój profil</a>
+                @endif
+                <a href="{{url('/friends')}}/{{$user->id}}">Znajomi</a>
+            </div>
+        </div>
+
+        <div class="card-body row">
+            <div class="col-12 col-md-3">
+                <h3 class="text-center"> Dane osobowe </h3>
+                <div class="row">
+                    <b> Imię: </b>  <span class="ml-auto"> {{$user->firstname}} </span>
+                </div>
+                <div class="row">
+                    <b>  Nazwisko: </b> <span class="ml-auto"> {{$user->lastname}} </span>
+                </div>
+                <div class="row">
+                    <b> Login: </b> <span class="ml-auto"> {{$user->login}} </span>
+                </div>
+                <div class="row">
+                    <b> Email: </b> <span class="ml-auto"> {{$user->email}} </span>
+                </div>
+                <div class="row">
+                    <b> Płeć: </b> <span class="ml-auto"> {{$user->profile->gender}} </span>
+                </div>
+                <div class="row">
+                    <b> Opis: </b> <span class="ml-auto">  {{$user->profile->description}} </span>
+                </div>
+            </div>
+            <div class="col-12 col-md-8 overflow-auto">
+                <h3 class="text-center"> Ostatnie wpisy </h3>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/users/settings.blade.php
+++ resources/views/users/settings.blade.php
@@ -1,65 +1,100 @@
 @extends('layouts.app')
 
 @section('content')
-	<h2>Personalizacja profilu</h2>
-	@if(count($errors) > 0)
-		<strong>Nie udało się przesłać pliku z powodu następujących błędów:</strong>
-		<ul>
-			@foreach($errors->all() as $error)
-				<li>{{$error}}</li>
-			@endforeach
-		</ul>
-	@else
-		@if(isset($success))
-			<p>{{$success}}</p>
-		@endif
-	@endif
-	<form action="{{route('settings.update')}}" method="POST" enctype="multipart/form-data">
-		<h3>Dane osobowe</h3>
-		<p>
-			<label for="fname">
-				Imię:
-			</label>
-			<input type="text" name="fname" id="fname" value = "{{old("fname", $user->firstname)}}">
-		</p>
-		<p>
-			<label for="lname">
-				Nazwisko:
-			</label>
-			<input type="text" name="lname" id="lname" value = "{{old("lname", $user->lastname)}}">
-		</p>
-		<p>
-			<label for="login">
-				Login:
-			</label>
-			<input type="text" name="login" id="login" value = "{{old("login", $user->login)}}">
-		</p>
-		<p>
-			<label for="email">
-				Email:
-			</label>
-			<input type="text" name="email" id="email" value = "{{old("email", $user->email)}}">
-		</p>
-		<h3>Informacje dodatkowe</h3>
-		<p>
-			<label for="gender">
-				Płeć:
-			</label>
-			<select name="gender" id="gender">
-				<option value="m">Mężczyzna</option>
-				<option value="f">Kobieta</option>
-			</select>
-		</p>
-		<h3>Aby inni mogli Cię lepiej poznać, dodaj tutaj kilka słów o sobie</h3>
-		<p>
-			<label for="desc">
-				Opowiedz innym o sobie:
-			</label>
-			<input type="text" id="desc" name="desc" value = "{{old("desc", $user->description)}}">
-		</p>
-		<h3>Zdjęcie profilowe</h3>
-		<input type='file' name='image' id='image'>
-		@csrf
-		<button type='submit'>Prześlij</button>
-	</form>
+    <div class="card">
+        <div class="card-header">
+            Personalizacja profilu
+        </div>
+        <div class="card-body">
+            @if(count($errors) > 0)
+                <strong>Nie udało się przesłać pliku z powodu następujących błędów:</strong>
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            @else
+                @if(isset($success))
+                    <p>{{$success}}</p>
+                @endif
+            @endif
+            <form action="{{route('settings.update')}}" method="POST" enctype="multipart/form-data"
+                  class="col-sm-10 mx-auto">
+                <fieldset>
+                    <legend> Dane osobowe</legend>
+                    <div class="row">
+                        <div class="col-6">
+                            <label for="fname" class="ml-2">
+                                Imię:
+                            </label>
+                            <input type="text" name="fname" id="fname" value="{{old("fname", $user->firstname)}}"
+                                   class="form-control">
+                        </div>
+                        <div class="col-6">
+                            <label for="lname" class="ml-2">
+                                Nazwisko:
+                            </label>
+                            <input type="text" name="lname" id="lname" value="{{old("lname", $user->lastname)}}"
+                                   class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="row my-2">
+                        <div class="col-6">
+                            <label for="login" class="ml-2">
+                                Login:
+                            </label>
+                            <input type="text" name="login" id="login" value="{{old("login", $user->login)}}"
+                                   class="form-control">
+                        </div>
+                        <div class="col-6">
+                            <label for="email" class="ml-2">
+                                Email:
+                            </label>
+                            <input type="text" name="email" id="email" value="{{old("email", $user->email)}}"
+                                   class="form-control">
+                        </div>
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <legend>Informacje dodatkowe</legend>
+                    <div class="row">
+                        <div class="col-4 text-center">
+                            <label for="gender">
+                                Płeć:
+                            </label>
+                        </div>
+                        <div class="col-8">
+                            <select name="gender" id="gender" class="custom-select">
+                                <option value="m">Mężczyzna</option>
+                                <option value="f">Kobieta</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row text-center">
+                        <div class="col-4">
+                            <label for = "image" class="my-auto"> Zdjęcie profilowe </label>
+                        </div>
+                        <div class="col-8">
+                            <input type='file' name='image' id='image' class="form-control">
+                        </div>
+                    </div>
+                    <h3 class="mt-4"> Aby inni mogli Cię lepiej poznać, dodaj tutaj kilka słów o sobie </h3>
+                    <div class="row">
+                        <label for="desc">
+                            Opowiedz innym o sobie:
+                        </label>
+                        <textarea type="text" id="desc" name="desc"
+                                  class="form-control"> {{old("desc", $user->profile->description)}} </textarea>
+                    </div>
+
+                    @csrf
+                    <div class="row my-3">
+                        <button type='submit' class="btn btn-success ml-auto mr-2"> Zapisz zmiany</button>
+                        <a href="{{ url("/users", [$user->id]) }}" class="btn btn-danger mx-auto"> Anuluj </a>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    </div>
 @endsection
